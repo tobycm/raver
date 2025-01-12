c442352256a1f32b2ce7f2ac31bd2be9c9963cd8
Toby Cm <toby@vsus.app>
2025-01-12 11:59:06 -0800

    top level await :)

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    target: "esnext",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            const packagePath = id.includes("node_modules/.pnpm")
+              ? id.toString().split("node_modules/.pnpm/")[1]
+              : id.toString().split("node_modules/")[1];
+
+            return id.includes("node_modules/.pnpm")
+              ? (packagePath[0] === "@" ? packagePath.replace("@", "") : packagePath).split("@")[0].toString()
+              : packagePath.split("/")[0].toString();
+          }
+
+          return id.split("/")[0];
+        },
+      },
+    },
+  },
+});
